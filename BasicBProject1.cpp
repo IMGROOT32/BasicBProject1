9401846870869346642cf25b20ddfb26d15539d7
IMGROOT32 <henrycho0302@gmail.com>
2026-02-19 11:27:47 +0900

    [실습] : 스마트 포인터 적용

--- BasicBProject1.cpp
+++ BasicBProject1.cpp
@@ -1,64 +1,33 @@
-﻿#include <iostream>
+﻿#include <Windows.h>
+#include <iostream>
+#include <memory>
+#include "BattleManager.h"
 #include "Character/Character.h"
-#include "Character/Player.h"
 #include "Character/Monster.h"
-#include <Windows.h>
+#include "Character/Player.h"
 
 using namespace std;
 
-void WaitForPlayerInput()
-{
-	system("pause");
-	cout << endl;
-}
-
-bool BattleTurn(ACharacter* Attacker, ACharacter* Defender)
-{
-		WaitForPlayerInput();
-
-
-		Attacker->PlayTurn(Defender);
-
-		Attacker->ShowStat();
-		Defender->ShowStat();
-		
-		if (Defender->IsDead())
-		{
-			cout << "몬스터가 쓰러졌습니다! 승리!" << endl;
-			return true;
-		}
-		
-		if (Attacker->IsDead())
-		{
-			cout << "플레이어가 쓰러졌습니다... 패배..." << endl;
-			return false;
-		}
-}
-
 int main()
 {
-	ACharacter* Player = new APlayer("나의 용사", FUnitStat(200, 50, 30, 5, 10));
-	ACharacter* Monster = new AMonster("무서운 오크", FUnitStat(100, 30, 20, 3, 10));
+	
+	auto BattleManager = make_unique<ABattleManager>();
+
+	FUnitStat PlayerStat(200, 50, 30, 5, 10);
+	FUnitStat MonsterStat(100, 30, 20, 3, 10);
+
+	unique_ptr<ACharacter> Player = make_unique<APlayer>("용사", PlayerStat);
+	unique_ptr<ACharacter> Monster = make_unique<AMonster>("몬스터", MonsterStat);
 
 	cout << "===  데스매치 시작!  ===" << endl;
 
-	while (true)
-	{
-		if (BattleTurn(Player, Monster) == true)
-		{
-			break;
-		}
+	BattleManager->WaitForPlayerInput();
+	BattleManager->RunBattle(Player.get(), Monster.get());
+	BattleManager->WaitForPlayerInput();
 
-		if (BattleTurn(Monster, Player) == true)
-		{
-			break;
-		}
-	}
+	cout << "===  데스매치 종료!  ===" << endl;
 
-	WaitForPlayerInput();
 
-	delete Player;
-	delete Monster;
 
 	return 0;
 }
---
+++ BattleManager.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+class ACharacter;
+
+class ABattleManager
+{
+public:
+	void RunBattle(ACharacter* charA, ACharacter* charB);
+	void WaitForPlayerInput();
+
+private:
+	bool BattleTurn(ACharacter* Attacker, ACharacter* Defender);
+};
+
+
